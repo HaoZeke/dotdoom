3dca82f668ab5a1b00b687e5fe28b95f7d33467a
HaoZeke <rohit.goswami@aol.com>
2019-04-27 04:39:13 +0530

    init: Fix warnings and enable fill-column

--- init.el
+++ init.el
@@ -19,9 +19,7 @@
       doom-dashboard    ; a nifty splash screen for Emacs
       ;; doom-modeline    ; a snazzy Atom-inspired mode-line
       ;; doom-quit         ; DOOM quit-message prompts when you quit Emacs
-      ;; fill-column       ; a `fill-column' indicator
-      evil-goggles      ; display visual hints when editing in evil
-      ;;fci               ; a `fill-column' indicator
+      fill-column       ; a `fill-column' indicator
       hl-todo           ; highlight TODO/FIXME/NOTE tags
       ;; indent-guides     ; highlighted indent columns
       modeline          ; snazzy, Atom-inspired modeline, plus API
