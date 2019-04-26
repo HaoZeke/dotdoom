fe6600d4445661a3294f4e34b365b10c1d0b4f9c
HaoZeke <rohit.goswami@aol.com>
2019-04-27 04:34:18 +0530

    init: Disable the write app

--- init.el
+++ init.el
@@ -160,9 +160,9 @@
       ;;irc               ; how neckbeards socialize
       ;;(rss +org)        ; emacs as an RSS reader
       ;;twitter           ; twitter client https://twitter.com/vnought
-      (write             ; emacs as a word processor (latex + org + markdown)
-      +wordnut           ; wordnet (wn) search
-      +langtool)         ; a proofreader (grammar/style check) for Emacs
+      ;; (write             ; emacs as a word processor (latex + org + markdown)
+      ;; +wordnut           ; wordnet (wn) search
+      ;; +langtool)         ; a proofreader (grammar/style check) for Emacs
 
        :collab
        ;;floobits          ; peer programming for a price
