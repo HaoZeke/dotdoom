9a853418d22b880d4203dfcdce3eb95880948f1d
HaoZeke <rohit.goswami@aol.com>
2019-05-19 17:53:22 +0530

    async: Fix and enable koma-article

--- local/async-ox.el
+++ local/async-ox.el
@@ -9,5 +9,15 @@
 (require 'cl)
 (setq org-export-async-debug nil)
 
+; For KOMAscript stuff
+(with-eval-after-load "ox-latex"
+  (add-to-list 'org-latex-classes
+               '("koma-article" "\\documentclass{scrartcl}"
+                 ("\\section{%s}" . "\\section*{%s}")
+                 ("\\subsection{%s}" . "\\subsection*{%s}")
+                 ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+                 ("\\paragraph{%s}" . "\\paragraph*{%s}")
+                 ("\\subparagraph{%s}" . "\\subparagraph*{%s}"))))
+
 (provide 'autoExport)
 ;;; autoExport.el ends here
