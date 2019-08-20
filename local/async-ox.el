897e2f557320c3e3edaa3bd151e58a436fec0466
HaoZeke <rohit.goswami@aol.com>
2019-08-20 08:32:43 +0530

    async: Commit generated async

--- local/async-ox.el
+++ local/async-ox.el
@@ -7,10 +7,26 @@
 (require 'org)
 (require 'ox)
 (require 'cl)
-(setq org-export-async-debug nil)
 
-; For KOMAscript stuff
-(with-eval-after-load "ox-latex"
+;; Feature parity with doom
+(with-eval-after-load 'ox-latex
+  ;; Compiler
+  (setq org-latex-pdf-process (list "latexmk -shell-escape -bibtex -f -pdfxe %f"))
+  ;; Configuration
+  (add-to-list 'org-latex-packages-alist '("" "minted" "xcolor"))
+  (setq org-latex-listings 'minted)
+  (setq org-latex-minted-options
+    '(("bgcolor" "black") ("linenos" "true") ("style" "native")))
+  (append-to-list
+   'org-latex-classes
+   '(("tufte-book"
+      "\\documentclass[a4paper, sfsidenotes, openany, justified]{tufte-book}
+      \\input{/home/haozeke/Git/tufte-book.tex}"
+      ("\\part{%s}" . "\\part*{%s}")
+      ("\\chapter{%s}" . "\\chapter*{%s}")
+      ("\\section{%s}" . "\\section*{%s}")
+      ("utf8" . "utf8x")
+      ("\\subsection{%s}" . "\\subsection*{%s}"))))
   (add-to-list 'org-latex-classes
                '("koma-article" "\\documentclass{scrartcl}"
                  ("\\section{%s}" . "\\section*{%s}")
@@ -18,12 +34,6 @@
                  ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
                  ("\\paragraph{%s}" . "\\paragraph*{%s}")
                  ("\\subparagraph{%s}" . "\\subparagraph*{%s}")))
-  )
-; Configuration
-(add-to-list 'org-latex-packages-alist '("" "minted" "xcolor"))
-(setq org-latex-minted-options
-  '(("bgcolor" "black") ("linenos" "true") ("style" "native")))
-(setq org-latex-listings 'minted)
-(setq org-latex-pdf-process (list "latexmk -shell-escape -bibtex -f -pdfxe %f"))
+)
 (provide 'autoExport)
 ;;; autoExport.el ends here
