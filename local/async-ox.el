5b90a2b61c84b4e127ff2575ddc62c49dbb36fe2
Rohit Goswami <rohit.goswami@aol.com>
2020-01-23 22:52:23 +0000

    config: Update latex listings

--- local/async-ox.el
+++ local/async-ox.el
@@ -29,7 +29,7 @@
   (add-to-list 'org-latex-packages-alist '("" "minted" "xcolor"))
   (setq org-latex-listings 'minted)
   (setq org-latex-minted-options
-    '(("bgcolor" "black") ("linenos" "true") ("style" "native")))
+    '(("bgcolor" "lightgray") ("linenos" "true") ("style" "tango")))
   (append-to-list
    'org-latex-classes
    '(("tufte-book"
