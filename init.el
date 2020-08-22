0cf17db0e7a37429276ecfcf2a31a9668055e841
Rohit Goswami <rgoswami@ieee.org>
2020-08-22 20:17:56 +0000

    init: Add ein

--- init.el
+++ init.el
@@ -99,7 +99,7 @@
        direnv
        ;; docker
        editorconfig      ; let someone else argue about tabs vs spaces
-       ;; ein            ; tame Jupyter notebooks with emacs
+       ein            ; tame Jupyter notebooks with emacs
        (eval +overlay)   ; run code, run (also, repls)
        gist              ; interacting with github gists
        (lookup           ; helps you navigate your code and documentation
