df05e4dc7dc4fc09da56ec9087ae3750e527f1a0
Rohit Goswami <rog32@hi.is>
2021-11-29 19:18:38 +0000

    ox: More conditional paths

--- local/async-ox.el
+++ local/async-ox.el
@@ -30,8 +30,13 @@
 (require 'org-ref)
 
 ;; Path addtion
-(setenv "PATH" (concat (getenv "PATH") ":/usr/local/texlive/2020/bin/x86_64-linux"))
-(setq exec-path (append exec-path '("/usr/local/texlive/2020/bin/x86_64-linux")))
+(cond (IS-LINUX (setenv "PATH" (concat (getenv "PATH") ":/usr/local/texlive/2020/bin/x86_64-linux"))
+                (setq exec-path (append exec-path '("/usr/local/texlive/2020/bin/x86_64-linux")))
+                )
+      (IS-MAC (setenv "PATH" (concat (getenv "PATH") ":/usr/local/texlive/2021/bin/universal-darwin"))
+              (setq exec-path (append exec-path '("/usr/local/texlive/2021/bin/universal-darwin")))
+              )
+      )
 
 ;; Functions
 ;; this function is used to append multiple elements to the list 'ox-latex
