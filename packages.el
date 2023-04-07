99177529aa61d41fb67394e7ad94522584287190
Rohit Goswami <rog32@hi.is>
2023-04-07 22:33:35 +0000

    MAINT: Pin things (bugfix)

--- packages.el
+++ packages.el
@@ -1,3 +1,9 @@
+;; [[file:packages.org::*Bugfixes][Bugfixes:1]]
+;; https://github.com/doomemacs/doomemacs/issues/7078#issuecomment-1430884107
+(package! transient :pin "c2bdf7e12c530eb85476d3aef317eb2941ab9440")
+(package! with-editor :pin "391e76a256aeec6b9e4cbd733088f30c677d965b")
+;; Bugfixes:1 ends here
+
 ;; [[file:packages.org::*MacOS][MacOS:1]]
 (cond (IS-MAC (package! exec-path-from-shell)
               ))
