a775a7037ab381199e50cbd3be96753dcbb109b4
Rohit Goswami <rog32@hi.is>
2025-08-28 15:33:50 +0200

    MAINT: Enable rust-analyzer

--- init.el
+++ init.el
@@ -216,7 +216,7 @@
        ;;rest               ; Emacs as a REST client
        rst                  ; ReST in peace
        ruby                 ; 1.step do {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
-       rust                 ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
+       (rust +lsp)          ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
        ;;scala              ; java, but good
        ;;(scheme +guile)    ; a fully conniving family of lisps
        (sh  +lsp)           ; she sells (ba|z)sh shells on the C xor
